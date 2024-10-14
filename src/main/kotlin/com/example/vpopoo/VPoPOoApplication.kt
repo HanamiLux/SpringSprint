72b6f867d4c3a87ea6a1b6b5d313d06c087d8d91
Nizovkin Zakhar Alexandrovich <oxotnikzn2017@yandex.ru>
2024-10-14 15:45:27 +0300

    Initial

---
+++ src/main/kotlin/com/example/vpopoo/VPoPOoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vpopoo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VPoPOoApplication
+
+fun main(args: Array<String>) {
+	runApplication<VPoPOoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/vpopoo/VPoPOoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.vpopoo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VPoPOoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
