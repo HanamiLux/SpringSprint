cee46f0610b57b4260c228b832fed018413a7c30
Nizovkin Zakhar Alexandrovich <oxotnikzn2017@yandex.ru>
2024-10-15 14:44:51 +0300

    Initial CRUD

---
+++ src/main/java/com/mpt/journal/VPoPOoApplication.java
@@ -0,0 +1,13 @@
+package com.mpt.journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VPoPOoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VPoPOoApplication.class, args);
+    }
+
+}
