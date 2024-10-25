39abc11f716b3738c07063da4ba7ae118747f75f
HanamiLux <oxotnikzn2017@yandex.ru>
2024-10-25 22:10:17 +0300

    Initial

---
+++ src/test/kotlin/api/vpopooapi/VPoPOoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package api.vpopooapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VPoPOoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
